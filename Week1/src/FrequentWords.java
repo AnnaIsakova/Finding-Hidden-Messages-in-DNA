0982daa43d5a7a7d9360399c638cb6a170a83c2d
Anna Isakova <anna.isakova@sap.com>
2018-12-05 17:49:30 +0200

    PatternCount

---
+++ Week1/src/FrequentWords.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FrequentWords {
+
+    public static List<String> frequentWords (String text, int k) {
+        List<String> result = new ArrayList<>();
+        HashMap<String, Integer> pairs = new HashMap<>();
+        return result;
+    }
+}
